725bc818e397fcccf2d6d2f3ae038d267f9f41b7
Craig Stowers <craig.stowers@outlook.com>
2024-05-28 12:11:25 +1000

    .

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const defaultData = {
 };
 
 const customInnerHeightBreakPoints = {
-   550: "short-inner-view",
+   450: "short-inner-view",
 };
 setInnerHeightCSSVariable(customInnerHeightBreakPoints);
 
