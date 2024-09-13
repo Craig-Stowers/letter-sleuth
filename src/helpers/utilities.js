d076721cbe6cee688c24356c7d19d530fea9e071
Craig Stowers <craig.stowers@outlook.com>
2024-09-13 12:40:17 +1000

    .

--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -1,35 +1,69 @@
+const screenAdjustTimers = [];
+
 const setInnerHeightCSSVariable = (breakpoints = {}) => {
-   let delayTimer = null;
-   function adjustHeight() {
-      const height = window.innerHeight;
-      const doc = document.documentElement;
-      var vh = height * 0.01;
-      document.documentElement.style.setProperty("--ivh", `${vh}px`);
+    let delayTimer = null;
+    function adjustHeight() {
+        const orientation =
+            window.screen &&
+            window.screen.orientation &&
+            window.screen.orientation.type;
 
-      Object.values(breakpoints).forEach((className) => {
-         doc.classList.remove(className);
-      });
-      for (const [breakpoint, className] of Object.entries(breakpoints)) {
-         if (height < breakpoint) {
-            doc.classList.add(className);
-            break;
-         }
-      }
-   }
+        const rotateDimensions = false; //orientation.includes("landscape");
 
-   const onScreenChange = () => {
-      if (delayTimer) clearTimeout(delayTimer);
-      adjustHeight();
-      delayTimer = setTimeout(() => {
-         adjustHeight();
-         clearTimeout(delayTimer);
-         delayTimer = null;
-      }, 1200);
-   };
-   // Listen for resize events
-   window.addEventListener("resize", onScreenChange);
-   window.addEventListener("orientationchange", onScreenChange);
-   onScreenChange();
+        console.log("indow.visualViewport", window.visualViewport);
+        const height = window.visualViewport
+            ? window.visualViewport.height
+            : window.innerHeight;
+        const width = window.visualViewport
+            ? window.visualViewport.width
+            : window.innerWidth;
+
+        const scale = window.visualViewport ? window.visualViewport.scale : 1;
+
+        console.log("screen height", window.screen.height, height);
+
+        const doc = document.documentElement;
+        var vh = height * scale * 0.01;
+        document.documentElement.style.setProperty("--ivh", `${vh}px`);
+
+        console.log("vh", vh);
+
+        Object.values(breakpoints).forEach((className) => {
+            doc.classList.remove(className);
+        });
+        for (const [breakpoint, className] of Object.entries(breakpoints)) {
+            if (height < breakpoint) {
+                doc.classList.add(className);
+                break;
+            }
+        }
+    }
+
+    const onScreenChange = () => {
+        if (delayTimer) clearTimeout(delayTimer);
+        adjustHeight();
+
+        for (let timer of screenAdjustTimers) {
+            clearTimeout(timer);
+        }
+
+        for (let i = 6; i > 0; i--) {
+            screenAdjustTimers.push(
+                setTimeout(() => {
+                    adjustHeight();
+                }, i * 250)
+            );
+        }
+    };
+    // Listen for resize events
+    window.addEventListener("resize", onScreenChange);
+    window.addEventListener("orientationchange", onScreenChange);
+    if (window.visualViewport) {
+        // window.visualViewport.addEventListener("resize", onScreenChange);
+        // window.visualViewport.addEventListener("scroll", onScreenChange); // Handle zooming effect during scroll
+    }
+
+    onScreenChange();
 };
 
 export { setInnerHeightCSSVariable };
